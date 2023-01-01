e62b89d2dd578477476ba7ef891fb961e9635e23
atillakokcu <kokcu.ati@gmail.com>
2023-01-01 19:18:52 +0300

    Kitap ve Yazar guncellemesi

--- MvcKutuphane/Controllers/KitapController.cs
+++ MvcKutuphane/Controllers/KitapController.cs
@@ -3,15 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcKutuphane.Models.Entity;
 
 namespace MvcKutuphane.Controllers
 {
     public class KitapController : Controller
     {
         // GET: Kitap
+        DbKutuphaneEntities db = new DbKutuphaneEntities();
         public ActionResult Index()
+        {
+            var Kitaplar = db.TblKitap.ToList();
+            return View(Kitaplar);
+        }
+        [HttpGet]
+        public ActionResult KitapEkle()
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult KitapEkle(TblKitap Kitap)
+        {
+            return View();
+        }
+
+
+
+
     }
 }
--- MvcKutuphane/Controllers/YazarController.cs
+++ MvcKutuphane/Controllers/YazarController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.Ajax.Utilities;
 using MvcKutuphane.Models.Entity;
 
 namespace MvcKutuphane.Controllers
@@ -10,7 +11,7 @@ namespace MvcKutuphane.Controllers
     public class YazarController : Controller
     {
         // GET: Yazar
-        DbKutuphaneEntities Db = new DbKutuphaneEntities(); 
+        DbKutuphaneEntities Db = new DbKutuphaneEntities();
         public ActionResult Index()
         {
             var YazarlarListesi = Db.TblYazar.ToList();
@@ -28,6 +29,31 @@ namespace MvcKutuphane.Controllers
             Db.TblYazar.Add(Yazar);
             Db.SaveChanges();
             return View();
+        }
+
+        public ActionResult YazarSil(int id)
+        {
+            var Yazar = Db.TblYazar.Find(id);
+            Db.TblYazar.Remove(Yazar);
+            Db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult YazarGetir(int Id)
+        {
+            var Yazar = Db.TblYazar.Find(Id);
+            return View("YazarGetir", Yazar);
+
+        }
+        public ActionResult YazarGuncelle(TblYazar Yazar)
+        {
+            var YazarDetay = Db.TblYazar.Find(Yazar.YazarId);
+            YazarDetay.YazarAdi = Yazar.YazarAdi;
+            YazarDetay.YazarSoyad = Yazar.YazarSoyad;
+            YazarDetay.YazarDetay = Yazar.YazarDetay;
+            Db.SaveChanges();
+            return RedirectToAction("Index");
+
         }
     }
 }
